a6b013c40dc085229cd9ac0c5e7079b5a2be9465
Emran Ahmed <emran.bd.08@gmail.com>
2025-01-22 21:55:06 +0600

    initial

---
+++ README.md
@@ -0,0 +1,34 @@
+# @storepress/create-plugin-html
+
+This is a template for [`@wordpress/create-block`](https://developer.wordpress.org/block-editor/reference-guides/packages/packages-create-block/) to create StorePress Plugin.
+
+## Usage
+
+This template can be used by running the following command:
+
+```bash
+npx @wordpress/create-block@latest --template @storepress/create-plugin-html my-extension-name
+```
+
+Navigate to the newly created folder and get started.
+
+```
+cd my-extension-name
+npm i                    # Install dependencies
+npm run build            # Build the javascript
+npx @wp-now/wp-now start # Start Wordpress environment
+```
+
+## Development
+
+For development on this tool itself, you can also install from a local directory.
+
+```bash
+npx @wordpress/create-block@latest --template ./path/to/storepress/create-plugin-html my-extension-name
+```
+
+### Add Tests
+
+Please check [Base Plugin](https://github.com/EmranAhmed/storepress-base-plugin/) to add `test` and others.
+
+This is a template to used with [`@wordpress/create-block`](https://developer.wordpress.org/block-editor/reference-guides/packages/packages-create-block/) to create a StorePress Plugin starting point.
---
+++ index.js
@@ -0,0 +1,144 @@
+/**
+ * External dependencies
+ */
+const { join } = require('path')
+
+// two-words
+function kebabCase (input) {
+  const regex = /[-._\s+]/gi
+  return input.replace(regex, '-').toLowerCase()
+}
+
+function constantCase (input) {
+  const regex = /[-._\s+]/gi
+  return input.replace(regex, '_').toUpperCase()
+}
+
+function pascalStorePress (input) {
+  return 'storepress' === input ? 'StorePress' : input
+}
+
+function pascalCase (input) {
+  return (input.match(/[a-zA-Z0-9]+/g) || []).map(w => `${w.charAt(0).toUpperCase()}${w.slice(1)}`).join('')
+}
+
+module.exports = {
+  defaultValues: {
+    folderName: 'src',
+    namespace: 'storepress',
+    slug: 'plugin',
+    version: '0.1.0',
+    title: 'StorePress Plugin',
+    description: 'A StorePress Plugin',
+    author: 'EmranAhmed',
+    dashicon: 'pets',
+    category: 'storepress',
+    attributes: {},
+    license: 'GPL-2.0-or-later',
+    customScripts: {
+      'postinstall': 'git init -q && rm -rf ./.husky && npx husky init && echo "npx lint-staged" > .husky/pre-commit',
+
+      'prebuild': 'rm -rf ./build',
+      'build': 'npm run start -- --no-watch && wp-scripts build --webpack-copy-php --experimental-modules',
+
+      'check-engines': 'wp-scripts check-engines',
+      'check-licenses': 'wp-scripts check-licenses',
+
+      'format': 'wp-scripts format ./src',
+      'format:all': 'npm run format:php && npm run format:css && npm run format:js',
+      'format:js': 'wp-scripts format \'./src/**/*.js\'',
+      'format:css': 'wp-scripts format \'./src/**/*.scss\'',
+
+      'lint:css': 'wp-scripts lint-style \'./src/**/*.scss\'',
+      'lint:css:report': 'npm run lint:css -- --output-file scss-report.txt',
+      'lint:css:fix': 'npm run lint:css -- --fix',
+
+      'lint:js': 'wp-scripts lint-js --format=pretty \'./src/**/*.js\'',
+      'lint:js:report': 'npm run lint:js -- --format html --output-file lint-report.html',
+      'lint:js:fix': 'npm run lint:js -- --fix',
+
+      'lint:md:docs': 'wp-scripts lint-md-docs',
+      'lint:pkg-json': 'wp-scripts lint-pkg-json',
+
+      'packages-update': 'wp-scripts packages-update',
+
+      'prepackage': 'rm -rf ./${npm_package_name}.zip && npm run build',
+      'package': './tools/package.js',
+
+      'plugin-zip': 'npm run package -- --zip',
+
+      'test:e2e': 'wp-scripts test-e2e',
+      'test:unit': 'wp-scripts test-unit-js',
+
+      'start': 'rm -rf ./build && wp-scripts start --webpack-copy-php --experimental-modules',
+    },
+    npmDependencies: [
+      '@storepress/utils',
+    ],
+    npmDevDependencies: [
+      '@wordpress/scripts',
+      '@woocommerce/dependency-extraction-webpack-plugin',
+      '@woocommerce/eslint-plugin',
+      '@wordpress/base-styles',
+      '@wordpress/dependency-extraction-webpack-plugin',
+      'eslint-plugin-you-dont-need-lodash-underscore',
+      'husky',
+      'lint-staged',
+      'fs-extra',
+      'webpack-remove-empty-scripts',
+      'eslint-plugin-prettier',
+      'eslint-formatter-pretty@5',
+    ],
+    customPackageJSON: {
+      'publishConfig': {
+        'access': 'public',
+      },
+      'sideEffects': [
+        'src/**',
+      ],
+      'lint-staged': {
+        './src/**/*.scss': [
+          'npm run lint:css',
+        ],
+        './src/**/*.{js,ts,tsx}': [
+          'npm run lint:js',
+        ],
+        './*.md': [
+          'npm run lint:md:docs',
+        ],
+        './package.json': [
+          'npm run lint:pkg-json',
+        ],
+      },
+      'main': 'build/index.js',
+      'module': 'src/index.js',
+      'files': [
+        'build/**',
+        'src/**',
+      ],
+      'bin': {
+        'package': './tools/package.js',
+      },
+    },
+    transformer: (view) => {
+      const todayDate = new Date().toJSON().slice(0, 10)
+      const pascaleNamespace = pascalCase(pascalStorePress(view.namespace))
+      const constantNamespace = constantCase(view.namespace)
+      const kebabNamespace = kebabCase(view.namespace)
+      const constantSlug = constantCase(view.slug) // TWO_WORDS
+      const kebabSlug = kebabCase(view.slug) // two-words
+      return {
+        ...view,
+        todayDate: todayDate,
+        constantNamespace: constantNamespace,
+        kebabNamespace: kebabNamespace,
+        pascaleNamespace: pascaleNamespace,
+
+        constantSlug: constantSlug,
+        kebabSlug: kebabSlug,
+      }
+    },
+  },
+  pluginTemplatesPath: join(__dirname, 'plugin-templates'),
+  blockTemplatesPath: join(__dirname, 'block-templates'),
+}
